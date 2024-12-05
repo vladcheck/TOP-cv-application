ee0b7fdb1270a54002ec7aac620f8795f69ebc4d
vladcheck <valekzhaninvladimir5@gmail.com>
2024-12-05 07:52:54 +0300

    Add form buttons

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -14,11 +14,13 @@ export function Form(props) {
       <InputRow label="Title of study" type="text" />
       <InputRow label="Date of study" type="date" />
       <h2>Experience</h2>
+      <button>Add experience</button>
       <InputRow label="Company" type="text" required="true" />
       <InputRow label="Position" type="text" required="true" />
       <InputRow label="Position responsibilities" type="text" required="true" />
       <InputRow label="From date" type="date" required="true" />
       <InputRow label="Until date" type="date" required="true" />
+      <button type="submit">Submit</button>
     </form>
   );
 }
