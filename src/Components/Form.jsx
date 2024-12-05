51df647de9ec23aaa08bf0a8d39198649527ac21
vladcheck <valekzhaninvladimir5@gmail.com>
2024-12-05 22:52:58 +0300

    Remove experience from the form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import ExperienceField from "./ExperienceField";
 import InputRow from "./Input";
 
 export function Form(props) {
-  let [experienceFields, setExperienceFields] = useState([]);
-
   return (
     <form method=".">
       <h1>Create your resume</h1>
@@ -17,17 +14,6 @@ export function Form(props) {
       <InputRow label="School name" type="text" required="true" />
       <InputRow label="Title of study" type="text" />
       <InputRow label="Date of study" type="date" />
-      <h2>Experience</h2>
-      <button
-        onClick={() => {
-          setExperienceFields([...experienceFields, ExperienceField()]);
-        }}
-      >
-        Add experience
-      </button>
-      <div id="experience-section">
-        {experienceFields.map((field) => field)}
-      </div>
       <button type="submit">Submit</button>
     </form>
   );
